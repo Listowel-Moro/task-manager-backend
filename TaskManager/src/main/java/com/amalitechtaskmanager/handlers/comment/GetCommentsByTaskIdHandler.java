beb7a1cdba227c33a2822d76cb6eb290e00e1e4a
mwayandau1 <mosesayandau1@gmail.com>
2025-04-29 14:58:53 +0000

    Refactored comment CRUD functionalities to make them much more cleaner

--- TaskManager/src/main/java/com/amalitechtaskmanager/handlers/comment/GetCommentsByTaskIdHandler.java
+++ TaskManager/src/main/java/com/amalitechtaskmanager/handlers/comment/GetCommentsByTaskIdHandler.java
@@ -84,22 +84,17 @@ public class GetCommentsByTaskIdHandler implements RequestHandler<APIGatewayProx
             return createResponse(400, "Error processing JSON: " + e.getMessage());
         } catch (DynamoDbException e) {
             context.getLogger().log("DynamoDB error: " + e.getMessage());
-            return createResponse(500, "Failed to retrieve comments: " + e.getMessage());
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(200)
+                    .withBody(objectMapper.writeValueAsString(responseBody))
+                    .withHeaders(Map.of("Content-Type", "application/json"));
         } catch (Exception e) {
             context.getLogger().log("Unexpected error: " + e.getMessage());
-            return createResponse(500, "Unexpected error occurred");
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(200)
+                    .withBody(objectMapper.writeValueAsString(responseBody))
+                    .withHeaders(Map.of("Content-Type", "application/json"));
         }
     }
 
-    private APIGatewayProxyResponseEvent createResponse(int statusCode, String body) {
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-        response.setStatusCode(statusCode);
-        response.setBody(body);
-
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        response.setHeaders(headers);
-
-        return response;
-    }
 }
