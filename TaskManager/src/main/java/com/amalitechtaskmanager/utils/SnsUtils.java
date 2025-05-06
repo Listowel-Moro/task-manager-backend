f0ec36b5caab94053bbca40dc950b7cb204613b4
rose-tetteh <rose.tetteh@amalitech.com>
2025-05-06 15:43:23 +0000

    task expiration feature modification

--- TaskManager/src/main/java/com/amalitechtaskmanager/utils/SnsUtils.java
+++ TaskManager/src/main/java/com/amalitechtaskmanager/utils/SnsUtils.java
@@ -26,7 +26,7 @@ public class SnsUtils {
                             .dataType("String")
                             .stringValue(email)
                             .build());
-            logger.info("messageAttributes" + messageAttributes);
+            logger.info("messageAttributes{}", messageAttributes);
             // Publish with message attributes
             SNSFactory.getSnsClient().publish(PublishRequest.builder()
                     .topicArn(topicArn)
@@ -39,37 +39,4 @@ public class SnsUtils {
             logger.error("Failed to send notification: {}", e.getMessage());
         }
     }
-
-
-    /**
-     * Send expiration email notification.
-     *
-     * @param snsClient the sns client
-     * @param topicArn  the topic arn
-     * @param email     the email
-     * @param subject   the subject
-     * @param message   the message
-     */
-    public static void sendExpirationEmailNotification(SnsClient snsClient, String topicArn, String email, String subject, String message) {
-        try {
-            Map<String, MessageAttributeValue> messageAttribute = new HashMap<>();
-            messageAttribute.put("recipient_email",
-                    MessageAttributeValue.builder()
-                            .dataType("String")
-                            .stringValue(email)
-                            .build());
-            logger.info("messageToBeSent: {}", messageAttribute);
-
-            snsClient.publish(PublishRequest.builder()
-                    .topicArn(topicArn)
-                    .message(message)
-                    .subject(subject)
-                    .messageAttributes(messageAttribute)
-                    .build());
-            logger.info("Email Notification sent to {}: {}", email, message);
-        } catch (Exception e) {
-            logger.error("Failed to send notification to {}: {}", email, e.getMessage());
-        }
-    }
-
 }
